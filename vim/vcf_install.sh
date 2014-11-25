51b748f3120d6b2a1c6d24f8ee6092fc2855cf1f
ssivakorn <ssivakorn@users.noreply.github.com>
2014-11-25 17:10:16 -0500

    download colorscheme directly

--- vim/vcf_install.sh
+++ vim/vcf_install.sh
@@ -10,10 +10,20 @@ _autoload_dir="autoload"
 echo "[>] Installing vim config ..."
 cp vimrc ~/.vimrc
 
-echo "[>] Installing color and syntax ..."
+echo "[>] Installing syntax ..."
 mkdir -p ~/${_vim_dir}
-cp -rf "$_colors_dir"  ~/"$_vim_dir"/.
+#cp -rf "$_colors_dir"  ~/"$_vim_dir"/.
 cp -rf "$_syntax_dir"  ~/"$_vim_dir"/.
+
+
+echo "[>] Installing my default colorschemes ..."
+mkdir -p ~/${_vim_dir}/${_colors_dir}
+curl -LSso ~/.vim/colors/luna-term.vim https://raw.githubusercontent.com/Pychimp/vim-luna/master/colors/luna-term.vim
+curl -LSso ~/.vim/colors/luna.vim https://raw.githubusercontent.com/Pychimp/vim-luna/master/colors/luna.vim
+curl -LSso ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -LSso ~/.vim/colors/256-jungle.vim https://raw.githubusercontent.com/vim-scripts/256-jungle/master/colors/256-jungle.vim
+
+
 
 echo "[>] Installing plugins and bundles ..."
 #cp -rf "$_plugin_dir" ~/"$_vim_dir"/.
