1019b3fe87202be6702c7cf66c9ffa420d7755a3
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2017-09-20 13:00:07 -0400

    change pager color

--- fish_shell/config.fish
+++ fish_shell/config.fish
@@ -3,27 +3,32 @@
 set fish_greeting
 
 # Set prompt variable color
-set fish_color_normal FFFFFF
-set fish_color_command FFFFFF
-set fish_color_param 71A4D9
-set fish_color_quote F9DA65
-set fish_color_redirection D75FAF
-set fish_color_end C793D7
-set fish_color_error FF0000 --bold
-set fish_color_comment 5F5F5F
-
-set fish_color_match FF00AE
-
-set fish_color_history_current cyan
-set fish color_search_match --background=87AFD7
-set fish_color_selection --bold --background=87AFD7
-
 set fish_color_autosuggestion 777777
-set fish_color_cancel FF0000 --bold
-set fish_color_escape 00DB16
-set fish_color_operator 00DB16
-
+set fish_color_cancel ff0000  --bold
+set fish_color_command ffffff
+set fish_color_comment 5f5f5f
+set fish_color_end 00d75f
+set fish_color_error ff0000  --bold
+set fish_color_escape 00db16
+set fish_color_history_current --background=87afd7
+set fish_color_match ff00ae
+set fish_color_normal ffffff
+set fish_color_operator 00db16
+set fish_color_param 71a4d9
+set fish_color_quote f9da65
+set fish_color_redirection d75faf
+set fish_color_search_match --background=87afd7
+set fish_color_selection 000000
 set fish_color_status red
 set fish_color_valid_path --underline
 
-set fish_color_cwd blue --bold
+#set fish_color_host -o  cyan
+#set fish_color_user -o  green
+#set fish_color_cwd_root red
+#set fish_color_cwd blue  --bold
+
+set fish_pager_color_prefix 18a5a6 cyan --underline
+set fish_pager_color_description 555555 darkgrey
+set fish_pager_color_progress 000000 --background=feff20
+#set fish_pager_color_completion
+#set fish_pager_color_secondary
