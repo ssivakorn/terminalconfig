5ec82aded6798b99cf1bd81861a8f8ce5131c3dd
Suphannee Sivakorn <ss6330@nyu.edu>
2017-09-20 11:21:55 -0400

    minor

---
+++ fish_shell/config.fish
@@ -0,0 +1,29 @@
+
+# Set greeting text
+set fish_greeting
+
+# Set prompt variable color
+set fish_color_normal FFFFFF
+set fish_color_command FFFFFF
+set fish_color_param 71A4D9
+set fish_color_quote F9DA65
+set fish_color_redirection D75FAF
+set fish_color_end C793D7
+set fish_color_error FF0000 --bold
+set fish_color_comment 5F5F5F
+
+set fish_color_match FF00AE
+
+set fish_color_history_current cyan
+set fish color_search_match --background=87AFD7
+set fish_color_selection --bold --background=87AFD7
+
+set fish_color_autosuggestion 777777
+set fish_color_cancel FF0000 --bold
+set fish_color_escape 00DB16
+set fish_color_operator 00DB16
+
+set fish_color_status red
+set fish_color_valid_path --underline
+
+set fish_color_cwd blue --bold
--- fish_shell/setup.sh
+++ fish_shell/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp fish_prompt.fish ~/.config/fish/functions/.
-fish config
+fish config.fish
