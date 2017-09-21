7258a42127a037d12043a5c50a65b7d1ee0533ef
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2017-09-21 01:32:17 -0400

    change colors

--- fish_shell/config.fish
+++ fish_shell/config.fish
@@ -7,17 +7,17 @@ set fish_color_autosuggestion 777777
 set fish_color_cancel ff0000  --bold
 set fish_color_command ffffff
 set fish_color_comment 5f5f5f
-set fish_color_end 00d75f
+set fish_color_end 00db16
 set fish_color_error ff0000  --bold
-set fish_color_escape 00db16
-set fish_color_history_current --background=87afd7
+set fish_color_escape af5fff
+set fish_color_history_current --background=303030
 set fish_color_match ff00ae
 set fish_color_normal ffffff
 set fish_color_operator 00db16
 set fish_color_param 71a4d9
 set fish_color_quote f9da65
 set fish_color_redirection d75faf
-set fish_color_search_match --background=87afd7
+set fish_color_search_match --background=303030
 set fish_color_selection 000000
 set fish_color_status red
 set fish_color_valid_path --underline
@@ -27,8 +27,9 @@ set fish_color_valid_path --underline
 #set fish_color_cwd_root red
 #set fish_color_cwd blue  --bold
 
-set fish_pager_color_prefix 18a5a6 cyan --underline
-set fish_pager_color_description 555555 darkgrey
+#set fish_pager_color_prefix 18a5a6 cyan --underline
+set fish_pager_color_prefix ffd75f --underline
+set fish_pager_color_description bcbcbc
 set fish_pager_color_progress 000000 --background=feff20
-#set fish_pager_color_completion
-#set fish_pager_color_secondary
+set fish_pager_color_completion FFFFFF
+set fish_pager_color_secondary FFFFFF
