54a4d1f52556d449017da4b11802d4e3c48e09d9
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2017-09-21 02:50:53 -0400

    update select color

--- fish_shell/config.fish
+++ fish_shell/config.fish
@@ -4,26 +4,26 @@ set fish_greeting
 
 # Set prompt variable color
 set fish_color_autosuggestion 777777
-set fish_color_cancel ff0000  --bold
+set fish_color_cancel ff0000 --bold
 set fish_color_command ffffff
 set fish_color_comment 5f5f5f
 set fish_color_end 00db16
-set fish_color_error ff0000  --bold
+set fish_color_error ff0000 --bold
 set fish_color_escape af5fff
-set fish_color_history_current --background=303030
+set fish_color_history_current --bold ffffff
 set fish_color_match ff00ae
 set fish_color_normal ffffff
 set fish_color_operator 00db16
 set fish_color_param 71a4d9
 set fish_color_quote f9da65
 set fish_color_redirection d75faf
-set fish_color_search_match --background=303030
+set fish_color_search_match --background=005faf
 set fish_color_selection 000000
-set fish_color_status red
+set fish_color_status ff0000
 set fish_color_valid_path --underline
 
-#set fish_color_host -o  cyan
 #set fish_color_user -o  green
+#set fish_color_host -o  cyan
 #set fish_color_cwd_root red
 #set fish_color_cwd blue  --bold
 
@@ -31,5 +31,5 @@ set fish_color_valid_path --underline
 set fish_pager_color_prefix ffd75f --underline
 set fish_pager_color_description bcbcbc
 set fish_pager_color_progress 000000 --background=feff20
-set fish_pager_color_completion FFFFFF
-set fish_pager_color_secondary FFFFFF
+set fish_pager_color_completion ffffff
+set fish_pager_color_secondary 18a5a6
