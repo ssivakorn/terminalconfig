c4187c1800fb7911736ca7ad9d626743c35f9a2e
Suphannee Sivakorn <ss6330@nyu.edu>
2017-09-25 15:45:42 -0400

    color minor

--- fish_shell/color_config.fish
+++ fish_shell/color_config.fish
@@ -1,18 +1,18 @@
 # Set prompt variable color
 set fish_color_autosuggestion 777777
-set fish_color_cancel ff0000 --bold
+set fish_color_cancel --bold ff0000
 set fish_color_command ffffff
 set fish_color_comment 5f5f5f
-set fish_color_end 00db16
-set fish_color_error ff0000 --bold
+set fish_color_end ff0087
+set fish_color_error --bold ff0000
 set fish_color_escape af5fff
 set fish_color_history_current --bold ffffff
 set fish_color_match ff00ae
 set fish_color_normal ffffff
 set fish_color_operator 00db16
-set fish_color_param 71a4d9
-set fish_color_quote f9da65
-set fish_color_redirection d75faf
+set fish_color_param 5fafff
+set fish_color_quote ffd700
+set fish_color_redirection 00d700
 set fish_color_search_match --background=363636
 set fish_color_selection 000000
 set fish_color_status ff0000
@@ -24,7 +24,7 @@ set fish_color_valid_path --underline
 #set fish_color_cwd blue  --bold
 
 #set fish_pager_color_prefix 18a5a6 cyan --underline
-set fish_pager_color_prefix ffd75f --underline
+set fish_pager_color_prefix --underline ffd75f
 set fish_pager_color_description bcbcbc
 set fish_pager_color_progress 000000 --background=feff20
 set fish_pager_color_completion ffffff
