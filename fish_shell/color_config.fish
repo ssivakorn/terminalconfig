d28e41239e478010c94f937ba5f8d3556d565bea
Suphannee Sivakorn <ss6330@nyu.edu>
2017-09-26 02:15:57 -0400

    minor

--- fish_shell/color_config.fish
+++ fish_shell/color_config.fish
@@ -3,7 +3,7 @@ set fish_color_autosuggestion 777777
 set fish_color_cancel --bold ff0000
 set fish_color_command ffffff
 set fish_color_comment 5f5f5f
-set fish_color_end ff0087
+set fish_color_end --bold ff0087
 set fish_color_error --bold ff0000
 set fish_color_escape af5fff
 set fish_color_history_current --bold ffffff
@@ -12,7 +12,7 @@ set fish_color_normal ffffff
 set fish_color_operator 00db16
 set fish_color_param 5fafff
 set fish_color_quote ffd700
-set fish_color_redirection 00d700
+set fish_color_redirection --bold 00d700
 set fish_color_search_match --background=363636
 set fish_color_selection 000000
 set fish_color_status ff0000
@@ -23,7 +23,6 @@ set fish_color_valid_path --underline
 #set fish_color_cwd_root red
 #set fish_color_cwd blue  --bold
 
-#set fish_pager_color_prefix 18a5a6 cyan --underline
 set fish_pager_color_prefix --underline ffd75f
 set fish_pager_color_description bcbcbc
 set fish_pager_color_progress 000000 --background=feff20
