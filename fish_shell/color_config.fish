cb853f7542122010923a000182ff0428fc3ec6d3
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2017-09-26 21:32:47 -0400

    update color

--- fish_shell/color_config.fish
+++ fish_shell/color_config.fish
@@ -10,9 +10,9 @@ set fish_color_history_current --bold ffffff
 set fish_color_match ff00ae
 set fish_color_normal ffffff
 set fish_color_operator 00db16
-set fish_color_param 5fafff
+set fish_color_param 0087ff
 set fish_color_quote ffd700
-set fish_color_redirection --bold 00d700
+set fish_color_redirection --bold ff8700
 set fish_color_search_match --background=363636
 set fish_color_selection 000000
 set fish_color_status ff0000
