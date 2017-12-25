7514e977883b3c030270b79204f3893590bc57c7
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2017-12-25 01:06:04 -0500

    update

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# TMUX plugins
 rm -rf ~/.tmux/plugins/tpm
 git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# TMUX configuration
 cp tmux.conf ~/.tmux.conf
+
+# Setup required:
+# https://github.com/tmux-plugins/tmux-yank
+# https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
