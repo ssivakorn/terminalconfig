b5e4ff5aa679447bfdb3afa80180ae7bcb59817f
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2018-10-24 23:44:24 -0400

    update fish colors

--- fish_shell/color_config.fish
+++ fish_shell/color_config.fish
@@ -1,8 +1,8 @@
-# Set prompt variable color
+# Set prompt variable colors
+
 set fish_color_autosuggestion 777777
 set fish_color_cancel --bold ff0000
 
-set fish_color_command normal
 set fish_color_comment 5f5f5f
 set fish_color_end --bold ff0087
 set fish_color_error --bold ff0000
@@ -10,14 +10,17 @@ set fish_color_escape af5fff
 #set fish_color_history_current --bold ffffff
 set fish_color_history_current --bold normal
 set fish_color_match ff00ae
-#set fish_color_normal ffffff
-set fish_color_normal normal
+
 set fish_color_operator 00db16
 set fish_color_param 0087ff
 set fish_color_quote ffd700
 set fish_color_redirection --bold ff8700
-set fish_color_search_match --background=414a4c
-set fish_color_selection --background=414a4c
+
+set fish_color_search_match --background=3a3e41
+set fish_color_selection --background=3a3e41
+#set fish_color_search_match --background=005faf
+#set fish_color_selection --background=005faf
+
 set fish_color_status ff0000
 set fish_color_valid_path --underline
 
@@ -26,9 +29,8 @@ set fish_color_valid_path --underline
 #set fish_color_cwd_root red
 #set fish_color_cwd blue  --bold
 
-set fish_pager_color_prefix --underline ffd75f
-set fish_pager_color_description bcbcbc
+set fish_pager_color_prefix --underline 0087ff
+set fish_pager_color_description -d bcbcbc
 set fish_pager_color_progress 000000 --background=feff20
-#set fish_pager_color_completion ffffff
 set fish_pager_color_completion normal
 set fish_pager_color_secondary 18a5a6
