95797eaeff0ab06f24c00c7c70ea3cabafedb804
ssivakorn <ss6330@nyu.edu>
2025-07-29 08:17:41 +0700

    minor:zsh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -6,11 +6,14 @@ path=$(pwd)
 # https://github.com/ohmyzsh/ohmyzsh
 rm -rf $HOME/.oh-my-zsh
 curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+rm $HOME/.zshrc
 ln -f -s $path/zshrc $HOME/.zshrc
 
 
 # Install/update plugins
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+
+mkdir -p $ZSH_CUSTOM
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
