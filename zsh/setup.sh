f09995d17f4433ace90037a0eb5243b0b96e9adc
ssivakorn <ss6330@nyu.edu>
2025-07-29 08:09:20 +0700

    Add zsh plugins

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -7,3 +7,10 @@ path=$(pwd)
 rm -rf $HOME/.oh-my-zsh
 curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 ln -f -s $path/zshrc $HOME/.zshrc
+
+
+# Install/update plugins
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
