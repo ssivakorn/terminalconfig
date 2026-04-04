8c3cfbca216531d4e6c05f86c94c4d6ce33a94e2
Suphannee Sivakorn <ssivakorn@users.noreply.github.com>
2026-04-04 08:43:54 -0400

    Update setup.sh

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -3,6 +3,6 @@
 path=$(pwd)
 
 # Ghostty configuration file
-$GHOSTTY_CONFIG=$HOME/.config/ghostty
+GHOSTTY_CONFIG=$HOME/.config/ghostty
 mkdir -p $GHOSTTY_CONFIG
 ln -f -s $path/config $GHOSTTY_CONFIG/config
